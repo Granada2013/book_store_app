758ed562fef6efb78172d472a8ec151f0cd590dc
Anna <example@gmail.com>
2020-10-22 22:12:58 +0700

    Spinner component added

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-bean-eater-dnx12f7rx4">
+      <div className="ldio-ybmcz69jwb">
+        <div><div></div><div></div><div></div></div><div><div></div><div></div><div></div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
