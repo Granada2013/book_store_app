ef6b73b3a616a85fac6dbc2ceb493bd3a548a5b4
Anna <example@gmail.com>
2020-10-23 19:33:49 +0700

    Shopping Cart component added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Container, Row, Col} from 'reactstrap';
 import CatalogList from '../catalogList';
 import ShoppingCart from '../shoppingCart';
+import ProductDetails from '../productDetails';
 import styled from 'styled-components';
 
 const NavBlock = styled.div`
@@ -17,26 +18,55 @@ const NavBlock = styled.div`
 
 export default class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      order: []
+  state = {
+      orders: [],
+      selectedItem: null,
+      isOpenModal: false
     };
-  }
+
+  onSelectItem = (selectedItem) => {
+    this.toggleModal();
+    this.setState({selectedItem});
+  };
+
+  toggleModal = () => {
+    const {isOpenModal} = this.state;
+    this.setState({
+      isOpenModal: !isOpenModal
+    });
+  };
+
+  onAddToCart = (amount) => {
+    const {selectedItem} = this.state;
+    const newOrder = {item: selectedItem.title, amount};
+    this.setState( state => {
+      return {
+        orders: [...state.orders, newOrder],
+        selectedItem: null
+      };
+    });
+    this.toggleModal();
+  };
 
   render() {
-    const {order} = this.state;
+    const {orders, selectedItem, isOpenModal} = this.state;
+    const modal = isOpenModal ? <ProductDetails
+                                  selectedItem={selectedItem}
+                                  isOpen={isOpenModal}
+                                  toggle={this.toggleModal}
+                                  onAddToCart={this.onAddToCart}/> : null;
     return (
       <>
-      <NavBlock>
-        <ShoppingCart number={order.length}/>
-      </NavBlock>
+        <NavBlock>
+          <ShoppingCart number={orders.length}/>
+        </NavBlock>
         <Container>
         <Row>
           <Col md="8">
-            <CatalogList/>
+            <CatalogList onSelectItem={this.onSelectItem}/>
           </Col>
         </Row>
+        {modal}
         </Container>
       </>
     );
