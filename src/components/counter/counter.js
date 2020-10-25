c83177e5c40ac1c4ab02ef88485adb28608b28a1
Anna <example@gmail.com>
2020-10-25 14:37:18 +0700

    Counter component added

---
+++ src/components/counter/counter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Input} from 'reactstrap';
+import styled from 'styled-components';
+
+const CustomInput = styled(Input)`
+  display: inline-block;
+  padding-right: 0;
+  width: 60px;
+  margin-left: 7px;
+  margin-right: 7px;
+`;
+
+const Counter = props => {
+  return (
+    <CustomInput id="amount" type="number" value={props.amount}
+           min="1" required
+           onInput={props.onChangeAmount}/>
+  );
+};
+export default Counter;
